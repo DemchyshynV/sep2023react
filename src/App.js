023b9688a70d64b8e2f09d471dbd585fc7b4603e
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2024-02-08 18:59:12 +0100

    lesson3/part1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import {useRef, useState} from "react";
+
 const App = () => {
+    // const [count, setCount] = useState(0)
+    const name = useRef();
+    const age = useRef();
+    const [user, setUser] = useState({})
+    const save = (e) => {
+      e.preventDefault()
+        // console.log(name.current.value);
+        // console.log(age.current.value);
+        console.log(e.target.name.value);
+        console.log(e.target.age.value);
+
+    }
+
+    console.log(user);
+    const handler = (e)=>{
+        e.preventDefault()
+        setUser(prevState => ({...prevState,[e.target.name]:e.target.value}))
+    }
+
     return (
         <div>
-            App
+            <form>
+                <input type="text" placeholder={'name'} name={'name'} onChange={handler} />
+                <input type="text" placeholder={'age'} name={'age'} onChange={handler} />
+                <button>save</button>
+            </form>
+            {/*<div>cont: {count}</div>*/}
+            {/*<button onClick={() => setCount(prev => prev + 1)}>inc</button>*/}
         </div>
     );
 };
